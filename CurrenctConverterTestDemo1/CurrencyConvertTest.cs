8a4c3873018763a0f78b8fe715bf855182acfc21
RaghavR2404 <rrast@danskeit.co.in>
2022-02-25 14:18:50 +0530

    Modified test cases

--- CurrenctConverterTestDemo1/CurrencyConvertTest.cs
+++ CurrenctConverterTestDemo1/CurrencyConvertTest.cs
@@ -7,15 +7,15 @@ namespace CurrenctConverterTestDemo1
 
         CurrencyConvert convert = new CurrencyConvert(new Rates(), new CheckInvalid(new Rates()));
         [Fact]
-        public void CheckExchangedAmount1()
+        public void CheckExchangedAmountIsNotMatching()
         {
             //fail
             decimal expected = 12m;
             decimal actual = convert.Calculate("EUR", "DKK", 1);
-            Assert.Equal(expected, actual);
+            Assert.NotEqual(expected, actual);
         }
         [Fact]
-        public void CheckExchangedAmount2()
+        public void CheckExchangedAmountIsMatching()
         {
             //pass
             decimal expected = 7.4394m;
@@ -23,16 +23,24 @@ namespace CurrenctConverterTestDemo1
             Assert.Equal(expected, actual);
         }
         [Theory]
-        [InlineData(-10)]
-        [InlineData(19)]
+        [InlineData(19.0)]
+        [InlineData(12.0)]
         public void CheckValidAmount(decimal amount)
         {
             Assert.True(amount > 0);
+        }
+        [Theory]
+        [InlineData(-10.0)]
+        [InlineData(-12.0)]   
+        public void CheckInValidAmount(decimal amount)
+        {
+            Assert.False(amount > 0);
         }
 
 
         [Theory]
         [InlineData("EUR","DNN",1)]
+        [InlineData("GBR","USD",22)]
         public void CheckUnknownTypeCurrency1(string currency1,string currency2,decimal amount)
         {
             Assert.Throws<System.Exception>(() =>  convert.Calculate(currency1, currency2, amount));
@@ -41,7 +49,8 @@ namespace CurrenctConverterTestDemo1
         [InlineData("EUR", "USD", 11)]
         public void CheckUnknownTypeCurrency2(string currency1, string currency2, decimal amount)
         {
-            Assert.Throws<System.Exception>(() => convert.Calculate(currency1, currency2, amount));
+           var ex =convert.Calculate(currency1, currency2, amount);
+           Assert.IsNotType<System.Exception>(() => ex);
         }
     }
 }
